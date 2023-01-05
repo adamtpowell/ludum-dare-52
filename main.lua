d9d171882235df17128dd7218d8c50a1eb062f18
Adam Tassara Powell <apowell@umass.edu>
2023-01-05 01:25:31 -0500

    Add terebi

--- main.lua
+++ main.lua
@@ -1,10 +1,20 @@
-function love.load()
+local terebi = require "libraries.terebi"
 
+function love.load()
+    terebi.initializeLoveDefaults()
+
+    Screen = terebi.newScreen(640, 490, 1)
+        :setBackgroundColor(0, 0, 0)
 end
 
 function love.update(dt)
 
 end
 
+function TerebiDraw()
+    -- Put drawing code here.
+end
+
 function love.draw()
+    Screen:draw(TerebiDraw)
 end
